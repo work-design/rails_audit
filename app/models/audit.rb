4a757f4dbca7929a668ba8636e4448eeb7e0d3d5
qinmingyuan <mingyuan0715@foxmail.com>
2017-03-20 14:41:23 +0800

    支持关联模型的变化及存储

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -1,9 +1,9 @@
 class Audit < ApplicationRecord
   serialize :audited_changes, Hash
+  serialize :related_changes, Hash
 
   belongs_to :auditable, polymorphic: true
   belongs_to :operator, polymorphic: true
-
 
   def audited_changes_i18n
     audited_changes.transform_keys { |key| auditable.class.human_attribute_name(key) }
--- app/models/auditable.rb
+++ app/models/auditable.rb
@@ -6,13 +6,40 @@ module Auditable
     has_many :audits, as: :auditable
   end
 
-  def save_audits(operator_type: 'User', operator_id: nil, note: '', previous: true)
+  # user: {
+  #   id: 1,
+  #   changes: {
+  #     name: ['a', 'b']
+  #   }
+  # }
+
+  def save_audits(operator_type: 'User', operator_id: nil, note: '', previous: true, include: [])
     audit = self.audits.build
     if previous
       audit.audited_changes = self.previous_changes.except(*IGNORE)
     else
       audit.audited_changes = self.changes
     end
+
+    if include.present?
+      result = {}
+      include.each do |key|
+        target = self.association(key).target
+
+        if previous
+          _changes = target.previous_changes.except(*IGNORE)
+        else
+          _changes = target.changes
+        end
+
+        result[key] = {
+          id: target.id,
+          changes: _changes
+        }
+      end
+      audit.related_changes = result
+    end
+
     audit.operator_type = operator_type
     audit.operator_id = operator_id
     audit.note = note
