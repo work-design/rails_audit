bb00b378c79d3205a49b7ea19fed90ef58c31678
qinmingyuan <mingyuan0715@foxmail.com>
2019-10-31 23:08:45 +0800

    simple logic

--- app/controllers/rails_audit/application.rb
+++ app/controllers/rails_audit/application.rb
@@ -1,26 +1,18 @@
 module RailsAudit::Application
 
-  def mark_audits(**options)
-    record_classes = options.select { |k, _|
-      record_class = k.to_s.safe_constantize
-      record_class && record_class.ancestors.include?(ActiveRecord::Base)
-    }
-
-    record_classes.each do |record_symbol, includes|
-      record_class = record_symbol.to_s.constantize
-      valid_ivars.find do |ivar|
-        record = instance_variable_get(ivar)
-        if record.is_a? record_class
-          record.save_audits(
-            current_operator: rails_audit_user,
-            include: includes,
-            note: options[:note],
-            controller_path: controller_path,
-            action_name: action_name,
-            remote_ip: request.remote_ip,
-            extra: options[:extra]
-          )
-        end
+  def mark_audits(record_class, **options)
+    valid_ivars.find do |ivar|
+      record = instance_variable_get(ivar)
+      if record.is_a? record_class
+        record.save_audits(
+          operator: rails_audit_user,
+          include: options[:include],
+          note: options[:note],
+          controller_path: controller_path,
+          action_name: action_name,
+          remote_ip: request.remote_ip,
+          extra: options[:extra]
+        )
       end
     end
   end
