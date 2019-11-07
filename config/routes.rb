d0490d0a3899fb5304ed38cfb8b191139e8aac22
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-07 16:00:30 +0800

    upgrade table

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
 
   scope ':audited_type/:audited_id', module: 'audit/admin' do
-    resources :audits, only: ['index']
+    resources :audits, only: [:index]
   end
 
   scope ':checking_type/:checking_id', module: 'audit/admin' do
     resources :checks
     resources :check_settings
+  end
+  
+  scope ':approving_type/:approving_id', module: 'audit/admin' do
+    resources :approvals, only: [:index]
+  end
+  
+  scope :admin, module: 'audit/admin', as: :admin do
+    resources :approvals, except: [:index]
   end
 
 end
