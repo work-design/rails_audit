a3cf97192122a0eda791d0a797949c4e44f9cdd3
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-26 11:48:46 +0800

    support both local or instance variables

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   include RailsAudit::Application
   # use after action, will auto record changes by use saved_changes api
   after_action only: [:update, :create, :destroy] do
-    mark_audits(:@user, :@role, note: 'note something!', extra: { client_headers: request.headers.as_json })
+    mark_audits(instance: [:@user, :@role], local: [:current_user], note: 'note something!', extra: { client_headers: request.headers.as_json })
   end
   
 end
--- app/controllers/rails_audit/application.rb
+++ app/controllers/rails_audit/application.rb
@@ -1,18 +1,30 @@
 module RailsAudit::Application
 
   # after_action
-  def mark_audits(*record_instances, **options)
-    record_instances.each do |ivar|
+  def mark_audits(instance: [], local: [], **options)
+    instance_records = Array(instance).map!(&:to_sym) & valid_ivars
+    instance_records.each do |ivar|
       record = instance_variable_get(ivar)
       next unless record.is_a?(ActiveRecord::Base)
-      record.save_audits(
-        operator: rails_audit_user,
-        controller_path: controller_path,
-        action_name: action_name,
-        remote_ip: request.remote_ip,
-        **options.slice(:only, :except, :include, :note, :extra)
-      )
+      save_audits(record, **options)
     end
+    
+    local_records = Array(local).map!(&:to_sym) & local_variables
+    local_records.each do |ivar|
+      record = eval(ivar)
+      next unless record.is_a?(ActiveRecord::Base)
+      save_audits(record, **options)
+    end
+  end
+  
+  def save_audits(record, **options)
+    record.save_audits(
+      operator: rails_audit_user,
+      controller_path: controller_path,
+      action_name: action_name,
+      remote_ip: request.remote_ip,
+      **options.slice(:only, :except, :include, :note, :extra)
+    )
   end
 
   def rails_audit_user
