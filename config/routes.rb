d8039189a8802d96ae4b24eb916310bb255ed2d1
qinmingyuan <mingyuan0715@foxmail.com>
2020-10-29 10:33:16 +0800

    add deafults with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  scope ':audited_type/:audited_id', module: 'audit/admin' do
+  scope ':audited_type/:audited_id', module: 'audit/admin', defaults: { namespace: 'admin', business: 'audit' } do
     resources :audits, only: [:index]
   end
 
-  scope ':approving_type/:approving_id', module: 'audit/admin' do
+  scope ':approving_type/:approving_id', module: 'audit/admin', defaults: { namespace: 'admin', business: 'audit' } do
     resources :approvals, only: [:index]
   end
 
-  scope :admin, module: 'audit/admin', as: :admin do
+  scope :admin, module: 'audit/admin', as: :admin, defaults: { namespace: 'admin', business: 'audit' } do
     resources :approvals, except: [:index, :new, :create]
     scope path: ':verifiable_type/:verifiable_id' do
       resources :verifiers do
