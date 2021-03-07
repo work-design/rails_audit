4564f257bcae74b39442b456ebf75acc838d05be
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-07 17:15:53 +0800

    update autidt

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  scope ':audited_type/:audited_id', module: 'auditor/admin', defaults: { namespace: 'admin', business: 'audit' } do
+  scope ':audited_type/:audited_id', module: 'auditor/admin', defaults: { business: 'auditor', namespace: 'admin' } do
     resources :audits, only: [:index]
   end
 
-  scope ':approving_type/:approving_id', module: 'auditor/admin', defaults: { namespace: 'admin', business: 'audit' } do
+  scope ':approving_type/:approving_id', module: 'auditor/admin', defaults: { business: 'auditor', namespace: 'admin' } do
     resources :approvals, only: [:index]
   end
 
-  scope :admin, module: 'auditor/admin', as: :admin, defaults: { namespace: 'admin', business: 'audit' } do
+  scope :admin, module: 'auditor/admin', as: :admin, defaults: { business: 'auditor', namespace: 'admin' } do
     resources :approvals, except: [:index, :new, :create]
     scope path: ':verifiable_type/:verifiable_id' do
       resources :verifiers do
