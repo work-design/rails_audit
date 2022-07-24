8909e617a2d8f17ef46b25364ac574b161212f65
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-24 20:02:50 +0800

    add default scope for

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
+  scope RailsExtend.default_routes_scope do
+    namespace :auditor, defaults: { business: 'auditor' } do
+      scope ':audited_type/:audited_id', module: 'admin', defaults: { namespace: 'admin' } do
+        resources :audits, only: [:index]
+      end
 
-  namespace :auditor, defaults: { business: 'auditor' } do
-    scope ':audited_type/:audited_id', module: 'admin', defaults: { namespace: 'admin' } do
-      resources :audits, only: [:index]
-    end
+      scope ':approving_type/:approving_id', module: 'admin', defaults: { namespace: 'admin' } do
+        resources :approvals, only: [:index]
+      end
 
-    scope ':approving_type/:approving_id', module: 'admin', defaults: { namespace: 'admin' } do
-      resources :approvals, only: [:index]
-    end
-
-    namespace :admin, defaults: { namespace: 'admin' } do
-      resources :approvals, except: [:index, :new, :create]
-      scope path: ':verifiable_type/:verifiable_id' do
-        resources :verifiers do
-          collection do
-            get :members
+      namespace :admin, defaults: { namespace: 'admin' } do
+        resources :approvals, except: [:index, :new, :create]
+        scope path: ':verifiable_type/:verifiable_id' do
+          resources :verifiers do
+            collection do
+              get :members
+            end
           end
         end
-      end
-      scope path: ':verified_type/:verified_id' do
-        resources :verifications
+        scope path: ':verified_type/:verified_id' do
+          resources :verifications
+        end
       end
     end
   end
-
 end
